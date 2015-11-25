8cd889809f3c5fd1275fd1df7e5efa08e6589b0b
John Pavley <john@pavley.com>
2015-11-25 14:35:35 -0500

    Add a zombie

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -10,7 +10,31 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    let zombie:SKSpriteNode = SKSpriteNode(imageNamed: "zombie1")
+    
     override func didMoveToView(view: SKView) {
+        
         backgroundColor = SKColor.blackColor()
+        
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        //background.anchorPoint = CGPoint.zero
+        //background.position = CGPoint.zero
+        //background.zRotation = CGFloat(M_PI) / 16
+        background.zPosition = -1
+        addChild(background)
+        
+        let mySize = background.size
+        print("size: \(mySize)")
+        
+        zombie.position = CGPoint(x: 400, y: 400)
+        //zombie.setScale(2.0)
+        addChild(zombie)
+        
+    }
+    
+    override func update(currentTime: NSTimeInterval) {
+        zombie.position = CGPoint(x: zombie.position.x + 8, y: zombie.position.y)
     }
 }
